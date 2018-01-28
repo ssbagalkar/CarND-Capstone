3c5db8ae96b9357f5d62c0f121439c103417b61d
Harry Wang <hgw2101@gmail.com>
2018-01-27 18:38:34 -0800

    incorporate tl_classifier into tl_deletector, classification is super slow (~15 seconds), and scores are very low, resulting in UNKNOWN traffic light classifications

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -16,6 +16,7 @@ STATE_COUNT_THRESHOLD = 3
 
 class TLDetector(object):
     def __init__(self):
+        print("initializing TLDetector!!!!!!!!")
         rospy.init_node('tl_detector', log_level=rospy.DEBUG)
 
         self.pose = None
@@ -128,9 +129,11 @@ class TLDetector(object):
         closest_distance = float('inf')
         for i in range(len(stop_line_positions)):
             distance_2D = self.distance([pose.position.x, pose.position.y], stop_line_positions[i])
-            if distance_2D < closest_distance:
+            if distance_2D < closest_distance and distance_2D < 5:
                 closest_distance = distance_2D
                 closest_stop_line = i
+        print("closest_distance")
+        print(closest_distance)
         return closest_stop_line
 
     def get_light_wp(self, stop_line_position):
@@ -162,7 +165,12 @@ class TLDetector(object):
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
+        print("calling get_light_state, this is light")
+        # print(light)
+
         #Get classification
+        # return TrafficLight.UNKNOWN
+
         return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
@@ -191,6 +199,7 @@ class TLDetector(object):
                 light_wp = self.get_light_wp(stop_line_positions[sl_pos_index])
 
         if light:
+            print("LIGHT!!!!!")
             state = self.get_light_state(light)
             return light_wp, state
         return -1, TrafficLight.UNKNOWN
